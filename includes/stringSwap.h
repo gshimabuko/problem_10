b7e633e4aecf8bab36d358699a55d209fbffcfff
gshimabuko <gshimabuko@gmail.com>
2021-11-08 12:40:09 -0300

    Created function to reverse string

---
+++ includes/stringSwap.h
@@ -0,0 +1,7 @@
+#ifndef __STRINGSWAP__
+#define __STRINGSWAP__
+
+#include <string>
+
+std::string strSwap(std::string s);
+#endif /* __STRINGSWAP__ */
---
+++ sources.mk
@@ -0,0 +1,4 @@
+SRC_DIR = src
+SOURCES =  $(SRC_DIR)/test.cpp \
+           $(SRC_DIR)/stringSwap.cpp
+INCLUDES = -I includes/
